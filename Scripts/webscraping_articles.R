06124fcb85c0529f690041c390866923913f7e98
Charlie Ikosi <charlieikosi@gmail.com>
2024-09-02 22:23:44 +1200

    Added Date formatting

--- Scripts/webscraping_articles.R
+++ Scripts/webscraping_articles.R
@@ -6,13 +6,16 @@ library(tidyverse)
 library(dplyr)
 
 # Check allowable number of pages to scrape
-#page_validate(1,"https://www.postcourier.com.pg/national-news/")
+page_validate(1,"https://www.postcourier.com.pg/sports/")
 #page_validate(1,"https://www.postcourier.com.pg/business/")
 
 # Load Current business articles df on github
 df_url <- "https://raw.githubusercontent.com/charlieikosi/PNG-News-Database/main/Postcourier/business_articles_pc.csv"
 df1 <- read_csv(df_url) %>%
   dplyr::select(2:4)
+
+# Scrape national news articles
+df1 <- scrape_news(2092, "national", "postcourier")
 
 # Scrape new business articles data
 df2 <- scrape_news(6, "business", "postcourier")
@@ -22,5 +25,8 @@ df2_filtered <- anti_join(df2, df1, by = c("Pub.Date", "Top.Stories", "URL"))
 
 # merge tables
 df1_merged <- rbind(df2_filtered, df1)
+
+# Set date column to date format
+df1_merged$Pub.Date <- as.POSIXct(df1_merged$Pub.Date, format = "%B %d, %Y")
 
 write.csv(df1_merged,"Postcourier/business_articles_pc.csv")
