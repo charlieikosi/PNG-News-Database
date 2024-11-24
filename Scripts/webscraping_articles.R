57cd90fabdda1125725564f7b3b3af1af207ba2e
Charlie Ikosi <charlieikosi@gmail.com>
2024-11-24 19:32:09 +1300

    Added Scripting for post-couriers national news category

--- Scripts/webscraping_articles.R
+++ Scripts/webscraping_articles.R
@@ -26,3 +26,23 @@ df2_filtered <- anti_join(df2, df1, by = c("Pub.Date", "Top.Stories", "URL"))
 df1_merged <- rbind(df2_filtered, df1)
 
 write.csv(df1_merged,"Postcourier/business_articles_pc.csv", row.names = FALSE)
+
+#------- Scrape National News Articles -----------------------------------------
+
+# Load Current business articles df on github
+#df_url <- "https://raw.githubusercontent.com/charlieikosi/PNG-News-Database/main/Postcourier/business_articles_pc.csv"
+#df1 <- read_csv(df_url) %>%
+#  na.omit()
+
+# Scrape new national news articles data
+df_pc.national <- scrape_news(460, "national", "postcourier")
+df_pc.national$Pub.Date <- as.POSIXct(strptime(df_pc.national$Pub.Date, format = "%B %d, %Y"))
+df_pc.national$Pub.Date <- df_pc.national$Pub.Date %>% as.Date(tryFormats = "%Y-%m-%d", tz = "NZ")
+
+# Filter out duplicate rows in scraped data
+df_pc.national.filtered <- anti_join(df_pc.national,old_df, by = c("Pub.Date", "Top.Stories", "URL"))
+
+# merge tables
+df_pc.national.merged <- rbind(df_pc.national,old_df)
+
+write.csv(df_pc.national.merged,"Postcourier/national_articles_pc.csv", row.names = FALSE)
