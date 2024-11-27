59b17abe9b59737ff977e74514b043a7e65b76fa
Charlie Ikosi <charlieikosi@gmail.com>
2024-11-27 16:54:59 +1300

    Updated

--- Scripts/webscraping_articles.R
+++ Scripts/webscraping_articles.R
@@ -30,19 +30,19 @@ write.csv(df1_merged,"Postcourier/business_articles_pc.csv", row.names = FALSE)
 #------- Scrape National News Articles -----------------------------------------
 
 # Load Current business articles df on github
-#df_url <- "https://raw.githubusercontent.com/charlieikosi/PNG-News-Database/main/Postcourier/business_articles_pc.csv"
-#df1 <- read_csv(df_url) %>%
-#  na.omit()
+df_pc.national_url <- "https://raw.githubusercontent.com/charlieikosi/PNG-News-Database/refs/heads/main/Postcourier/national_articles_pc.csv"
+df1_pc.national <- read_csv(df_pc.national_url) %>%
+  na.omit()
 
 # Scrape new national news articles data
-df_pc.national <- scrape_news(460, "national", "postcourier")
-df_pc.national$Pub.Date <- as.POSIXct(strptime(df_pc.national$Pub.Date, format = "%B %d, %Y"))
-df_pc.national$Pub.Date <- df_pc.national$Pub.Date %>% as.Date(tryFormats = "%Y-%m-%d", tz = "NZ")
+df2_pc.national <- scrape_news(10, "national", "postcourier")
+df2_pc.national$Pub.Date <- as.POSIXct(strptime(df2_pc.national$Pub.Date, format = "%B %d, %Y"))
+df2_pc.national$Pub.Date <- df2_pc.national$Pub.Date %>% as.Date(tryFormats = "%Y-%m-%d", tz = "NZ")
 
 # Filter out duplicate rows in scraped data
-df_pc.national.filtered <- anti_join(df_pc.national,old_df, by = c("Pub.Date", "Top.Stories", "URL"))
+df_pc.national.filtered <- anti_join(df2_pc.national,df1_pc.national, by = c("Pub.Date", "Top.Stories", "URL"))
 
 # merge tables
-df_pc.national.merged <- rbind(df_pc.national,old_df)
+df_pc.national.merged <- rbind(df_pc.national.filtered,df1_pc.national)
 
 write.csv(df_pc.national.merged,"Postcourier/national_articles_pc.csv", row.names = FALSE)
